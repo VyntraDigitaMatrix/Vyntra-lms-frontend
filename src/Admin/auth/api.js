03f40d52d988365dc80d199156de78dad1bb417d
Sathya7032 <sathichary581@gmail.com>
2026-08-01 13:05:31 +0530

    api changes

--- src/Admin/auth/api.js
+++ src/Admin/auth/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://200.97.164.120:8080";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://vyntra.swipengrow.in";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
